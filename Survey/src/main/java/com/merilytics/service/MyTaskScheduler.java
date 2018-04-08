8acd872cb0d152e5f7671b7a7c63c9b9132f9415
sameerbsau <sameerbsau@users.noreply.github.com>
2018-04-08 21:15:18 +0530

    pool properties has been specified

--- Survey/src/main/java/com/merilytics/service/MyTaskScheduler.java
+++ Survey/src/main/java/com/merilytics/service/MyTaskScheduler.java
@@ -1,18 +1,30 @@
 package com.merilytics.service;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 @EnableAsync
 @EnableScheduling
 public class MyTaskScheduler {
-	@Scheduled(fixedDelay=5000)
+	@Scheduled(fixedDelay = 5000)
 	public void doSomething() {
-	    // something that should execute periodically
-		
+		// something that should execute periodically
+
 		System.out.println("yay.... i am enjoying here");
 	}
+	
+	@Bean
+    public TaskScheduler poolScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setThreadNamePrefix("poolScheduler");
+        scheduler.setPoolSize(10);
+        return scheduler;
+    }
+	
 }
